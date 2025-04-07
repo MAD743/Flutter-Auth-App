f8eb440da55b42942fdab2a3b160dbd6e6ef3122
Win Nguyen <winnguyen@Wins-MBP.attlocal.net>
2025-04-07 14:20:32 -0400

    main dart, functionality implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,242 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: AuthScreen()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// Screen that lets you Register or Login
+class AuthScreen extends StatefulWidget {
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
 
-  // This widget is the root of your application.
+class _AuthScreenState extends State<AuthScreen> {
+  bool showLogin = true;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return Scaffold(
+      appBar: AppBar(title: Text('Auth App')),
+      body: Center(
+        child:
+            showLogin
+                ? LoginForm(switchView: toggleView)
+                : RegisterForm(switchView: toggleView),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+
+  void toggleView() {
+    setState(() {
+      showLogin = !showLogin;
+    });
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  final VoidCallback switchView;
+  RegisterForm({required this.switchView});
+
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final _email = TextEditingController();
+  final _pass = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String msg = '';
+
+  void register() async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _email.text,
+        password: _pass.text,
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (ctx) => ProfileScreen()),
+      );
+    } catch (e) {
+      setState(() {
+        msg = "Error: ${e.toString()}";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(24),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Register", style: TextStyle(fontSize: 20)),
+            TextFormField(
+              controller: _email,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              controller: _pass,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) register();
+              },
+              child: Text("Register"),
+            ),
+            TextButton(
+              onPressed: widget.switchView,
+              child: Text("Already have an account? Log in"),
+            ),
+            Text(msg, style: TextStyle(color: Colors.red)),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginForm extends StatefulWidget {
+  final VoidCallback switchView;
+  LoginForm({required this.switchView});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginForm> createState() => _LoginFormState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginFormState extends State<LoginForm> {
+  final _email = TextEditingController();
+  final _pass = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String msg = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void login() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _email.text,
+        password: _pass.text,
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (ctx) => ProfileScreen()),
+      );
+    } catch (e) {
+      setState(() {
+        msg = "Login failed";
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Padding(
+      padding: EdgeInsets.all(24),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Login", style: TextStyle(fontSize: 20)),
+            TextFormField(
+              controller: _email,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              controller: _pass,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) login();
+              },
+              child: Text("Login"),
+            ),
+            TextButton(
+              onPressed: widget.switchView,
+              child: Text("Don't have an account? Register"),
+            ),
+            Text(msg, style: TextStyle(color: Colors.red)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Profile screen after login
+class ProfileScreen extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser;
+
+  void logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (ctx) => AuthScreen()),
+    );
+  }
+
+  void changePassword(BuildContext context) async {
+    final newPass = TextEditingController();
+    showDialog(
+      context: context,
+      builder:
+          (_) => AlertDialog(
+            title: Text("Change Password"),
+            content: TextField(
+              controller: newPass,
+              obscureText: true,
+              decoration: InputDecoration(labelText: "New Password"),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  if (newPass.text.length >= 6) {
+                    await user?.updatePassword(newPass.text);
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text("Change"),
+              ),
+            ],
+          ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Welcome!"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () => logout(context),
+          ),
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Logged in as: ${user?.email}"),
+            ElevatedButton(
+              onPressed: () => changePassword(context),
+              child: Text("Change Password"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
